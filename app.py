622cda44432494e588d35d70fcb66cb7fa587fbf
mikeasilva <mike.a.silva@gmail.com>
2021-03-28 14:29:19 -0400

    Added first game

--- app.py
+++ app.py
@@ -2,9 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Based off of http://phaser.io/tutorials/getting-started-phaser3/
 @app.route("/getting-started")
 def getting_started():
     return render_template("getting_started.html")
+
+# Based off of http://phaser.io/tutorials/making-your-first-phaser-3-game/
+@app.route("/first-game")
+def first_game():
+    return render_template("first_game.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
